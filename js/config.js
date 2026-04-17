86f5285a8bb75b3f650a1c6e898677d3cb012dd0
Anjalikhullar <akhullar@squareup.com>
2026-04-17 14:22:29 -0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+// Airtable Configuration
+const CONFIG = {
+    AIRTABLE_TOKEN: 'YOUR_AIRTABLE_TOKEN_HERE',
+    BASE_ID: 'appfmmEsdda8cIfyy',
+    TABLE_ID: 'tbl3DZrf4jk5UnqgS',
+    REFRESH_INTERVAL: 30 * 60 * 1000
+};
