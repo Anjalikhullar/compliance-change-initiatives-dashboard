0c29d3ab2c33fe599c455c8041066ee12d0d2812
Anjalikhullar <akhullar@squareup.com>
2026-04-17 15:28:15 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Airtable Configuration
 const CONFIG = {
-    AIRTABLE_TOKEN: 'YOUR_AIRTABLE_TOKEN_HERE',
+    AIRTABLE_TOKEN: 'patoMUr6We6iZPs9G.6f9a0cc0ce6778714b1fc031a6f0b69496989209c9fd28a5236e9cd416de0099',
     BASE_ID: 'appfmmEsdda8cIfyy',
     TABLE_ID: 'tbl3DZrf4jk5UnqgS',
     REFRESH_INTERVAL: 30 * 60 * 1000
